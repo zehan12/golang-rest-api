53edb3f92c5700e59c34df3ac514c5db70c4c8a7
Zehan Khan <zkhan@hootboard.com>
2023-11-08 01:33:36 +0530

    feat: :sparkles: added the controllers

---
+++ controllers/user.go
@@ -0,0 +1,92 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang-rest-api/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
+	u := models.User{}
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+
+	uc.session.DB("mongo-golang").C("users").Insert(u)
+
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	err := uc.session.DB("mongo-golang").C("users").RemoveId(oid)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Deleted user with ID: %s\n", oid.Hex())
+}
--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
-import {
-	"github.com/julienschmidt/httprouter",
-	"gopkg.in/mgo.v2/",
-	"net/http",
-	"fmt",
+import (
+	"fmt"
+	"golang-rest-api/controllers"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	http.ListenAndServe("localhost:9000", r)
 }
 
-func main{
-	fmt.Println(import)
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mangodb://localhost:27107")
+	if err == nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	if s != nil {
+		fmt.Println(s)
+	}
+	return s
 }
-
-main()
